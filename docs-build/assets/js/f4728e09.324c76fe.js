22ec07857046c365d5e8c40ed31ce34baac89d81
Govind Yadav <govindy.enjay@gmail.com>
2024-07-20 11:39:08 +0530

    docs build file for github pages

---
+++ docs-build/assets/js/f4728e09.324c76fe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[9502],{7957:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/personal-blogs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
